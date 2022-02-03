67cf76fd8b4f89e16d72ddef2b18381c912885d2
Morgane Lailler <morgane.lailler.pro@gmail.com>
2022-02-03 11:02:09 +0100

    finished cart

--- src/Cart/CartService.php
+++ src/Cart/CartService.php
@@ -2,6 +2,7 @@
 
 namespace App\Cart;
 
+use App\Cart\CartItem;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
@@ -16,24 +17,60 @@ class CartService
         $this->productRepository = $productRepository;
     }
 
+    protected function getCart(): array
+    {
+        return $this->session->get('cart', []);
+    }
+
+    protected function saveCart(array $cart): void
+    {
+        $this->session->set('cart', $cart);
+    }
+
     public function add(int $id): void
     {
-        $cart = $this->session->get('cart', []);
+        $cart = $this->getCart();
 
         if (is_array($cart) && array_key_exists($id, $cart)) {
             $cart[$id]++;
         } else {
             $cart[$id] = 1;
         }
-        $this->session->set('cart', $cart);
+        $this->saveCart($cart);
+    }
+
+    public function remove(int $id): void
+    {
+        $cart = $this->getCart();
+        unset($cart[$id]);
+        $this->saveCart($cart);
+    }
+
+    public function decrement(int $id): void
+    {
+        $cart = $this->getCart();
+        if (is_array($cart) && !array_key_exists($id, $cart)) {
+            return;
+        }
+        if ($cart[$id] === 1) {
+            unset($cart[$id]);
+        } else {
+            $cart[$id]--;
+        }
+        $this->saveCart($cart);
     }
 
     public function getTotal(): int
     {
         $total = 0;
 
-        foreach ($this->session->get('cart', []) as $id => $quantity) {
+        foreach ($this->getCart() as $id => $quantity) {
             $product = $this->productRepository->find($id);
+
+            if ($product == null) {
+                continue;
+            }
+
             $total += $product->getPrice() * $quantity;
         }
         return $total;
@@ -42,13 +79,15 @@ class CartService
     public function getDetailedCartItems(): array
     {
         $detailedCart = [];
-        $cart = $this->session->get('cart', []);
+        $cart = $this->getCart();
         foreach ($cart as $id => $quantity) {
             $product = $this->productRepository->find($id);
-            $detailedCart[] = [
-                'product' => $product,
-                'quantity' => $quantity
-            ];
+
+            if ($product == null) {
+                continue;
+            }
+
+            $detailedCart[] = new CartItem($product, $quantity);
         }
         return $detailedCart;
     }
