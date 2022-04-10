55179c74e8062c898303417a2c0423c0b7c21faf
Morgane Lailler <morgane.lailler.pro@gmail.com>
2022-04-10 21:14:23 +0200

    add static pages

--- assets/app.js
+++ assets/app.js
@@ -20,6 +20,6 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
